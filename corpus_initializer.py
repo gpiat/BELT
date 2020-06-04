922fcc7a87b113967d3cc63564862daa252d9e0a
gpiat <guilhem.piat@cea.fr>
2020-06-04 16:19:39 +0200

    Increased command line parameter support

--- corpus_initializer.py
+++ corpus_initializer.py
@@ -21,14 +21,14 @@ def _UMLS_concepts_initializer(corpus, dct):
     #   full default category implementation
     # umls_concepts = {cuid: (index + 2)
     #                  for index, cuid in enumerate(umls_concepts)}
-    # umls_concepts["other"] = 1
+    umls_concepts["other"] = 1
 
     # the zeroth concept is the non-concept.
     umls_concepts[None] = 0
     return umls_concepts
 
 
-def UMLS_concepts_init(outfile=cst.umls_fname,
+def UMLS_concepts_init(fnames,
                        corpora=None, dflt_cat_thresh=0):
     """ Pickles a dictionary mapping CUIDs found in a list of corpora to indices.
         Args:
@@ -40,14 +40,14 @@ def UMLS_concepts_init(outfile=cst.umls_fname,
                 this threshold is put in a "default" category.
     """
     if corpora is None:
-        corpora = [cst.full_corpus_fname]
+        corpora = [fnames["--full_corpus_fname"]]
     corpus = MedMentionsCorpus(corpora)
     umls_concepts = _UMLS_concepts_initializer(corpus, dflt_cat_thresh)
-    with open(outfile, 'wb') as f:
+    with open(fnames["--umls_fname"], 'wb') as f:
         pickle.dump(umls_concepts, f)
 
 
-def create_corpora():
+def create_corpora(fnames):
     """ Splits the full corpus given in constants.py into training, validation,
         and test corpora. The proportions of documents going into each corpus
         are set in constants.py as well. These smaller corpora are written to
@@ -55,58 +55,84 @@ def create_corpora():
         PubTator format.
     """
     # creating sub-corpora for train, test and validation
-    full_corpus = MedMentionsCorpus([cst.full_corpus_fname])
+    full_corpus = MedMentionsCorpus([fnames['--full_corpus_fname']])
 
     pmids = {}
-    with open(cst.train_corpus_pmids, 'r') as f:
+    with open(fnames['--train_corpus_pmids'], 'r') as f:
         pmids['train'] = [pmid.strip() for pmid in f.readlines()]
-    with open(cst.val_corpus_pmids, 'r') as f:
+    with open(fnames['--val_corpus_pmids'], 'r') as f:
         pmids['val'] = [pmid.strip() for pmid in f.readlines()]
-    with open(cst.test_corpus_pmids, 'r') as f:
+    with open(fnames['--test_corpus_pmids'], 'r') as f:
         pmids['test'] = [pmid.strip() for pmid in f.readlines()]
 
     for document in full_corpus.documents():
         if document.pmid in pmids['train']:
-            document.write_to(cst.med_corpus_train)
+            document.write_to(fnames['--med_corpus_train'])
         elif document.pmid in pmids['val']:
-            document.write_to(cst.med_corpus_val)
+            document.write_to(fnames['--med_corpus_val'])
         elif document.pmid in pmids['test']:
-            document.write_to(cst.med_corpus_test)
+            document.write_to(fnames['--med_corpus_test'])
 
 
-def pickle_corpora():
+def pickle_corpora(fnames):
     """ Creates MedMentionsCorpus objects based on the train, validation and
         test corpora whose filenames are set in constants.py, which are then
         pickled. This avoids object creation overhead (which is fairly
         expensive) when running many experiments on the same corpus.
     """
-    train_corpus = MedMentionsCorpus([cst.med_corpus_train], auto_looping=True)
-    val_corpus = MedMentionsCorpus([cst.med_corpus_val])
-    test_corpus = MedMentionsCorpus([cst.med_corpus_test])
+    train_corpus = MedMentionsCorpus([fnames['--med_corpus_train']],
+                                     auto_looping=True)
+    val_corpus = MedMentionsCorpus([fnames['--med_corpus_val']])
+    test_corpus = MedMentionsCorpus([fnames['--med_corpus_test']])
 
-    with open(cst.train_fname, 'wb') as train_file:
+    with open(fnames['--train_fname'], 'wb') as train_file:
         pickle.dump(train_corpus, train_file)
-    with open(cst.val_fname, 'wb') as val_file:
+    with open(fnames['--val_fname'], 'wb') as val_file:
         pickle.dump(val_corpus, val_file)
-    with open(cst.test_fname, 'wb') as test_file:
+    with open(fnames['--test_fname'], 'wb') as test_file:
         pickle.dump(test_corpus, test_file)
 
 
 if __name__ == '__main__':
+
+    args = {
+        "--full_corpus_fname": cst.full_corpus_fname,
+        "--train_corpus_pmids": cst.train_corpus_pmids,
+        "--val_corpus_pmids": cst.val_corpus_pmids,
+        "--test_corpus_pmids": cst.test_corpus_pmids,
+
+        # create_corpora specific
+        "--med_corpus_train": cst.med_corpus_train,
+        "--med_corpus_val": cst.med_corpus_val,
+        "--med_corpus_test": cst.med_corpus_test,
+
+        # pickle_corpora specific
+        "--train_fname": cst.train_fname,
+        "--val_fname": cst.val_fname,
+        "--test_fname": cst.test_fname,
+
+        # UMLS_concepts_init specific
+        "--umls_fname": cst.umls_fname
+    }
+    for i in range(len(argv)):
+        arg = argv[i]
+        if arg in args.keys():
+            args[arg] = argv[i + 1]
+
     no_valid_args = True
 
     if "--create" in argv:
         no_valid_args = False
-        create_corpora()
+        create_corpora(fnames=args)
 
     if "--pickle" in argv:
         no_valid_args = False
-        pickle_corpora()
+        pickle_corpora(fnames=args)
         dct = 0
         if "--dct" in argv:
             # number that (hopefully) comes after "--dct" in argv
             dct = int(argv[argv.index("--dct") + 1])
-        UMLS_concepts_init(dflt_cat_thresh=dct)
+        UMLS_concepts_init(fnames=args, dflt_cat_thresh=dct)
 
     if no_valid_args:
         print("No valid arguments passed.\n"
