d36908f2bd07ec0ac899f6e38e5eaeb303632605
gpiat <guilhem.piat@cea.fr>
2020-07-20 15:40:34 +0200

    removed debug print statement

--- corpus_initializer.py
+++ corpus_initializer.py
@@ -60,7 +60,6 @@ def semantic_types_init(fnames, corpora=None):
                       for index, (stid, _) in
                       enumerate(corpus.stids.items())}
     semantic_types[None] = 0
-    print(semantic_types)
     with open(fnames["--st21_fname"], 'wb') as f:
         pickle.dump(semantic_types, f)
 
