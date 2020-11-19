dc7bd0edc512cf1cd9c2355c9626a0c667ab5b92
gpiat <guilhem.piat@cea.fr>
2020-11-19 17:36:37 +0100

    Various minor improvements and bugfix Changelog:     - added / deleted / edited comments     - bugfix: a thing that needed to be done once was in a loop     - bugfix: loop looped too many times, causing the model to train more than intended     - code refactoring: deleted some redundant safety measures that just complicated the code, renamed some variables, changed the order of some instructions to make it flow more "normally"     - commented out some obsolete debugging code

--- train.py
+++ train.py
@@ -34,26 +34,52 @@ def train(model, corpus, target_finder, target_indexing, optimizer,
             scheduler
             batch_size (int): number of batches of text to handle
                 simultaneously
-            overlap (float in [0,1]): proportion of the windows that should
+            overlap (float in [0,1[): proportion of the windows that should
                 overlap
             epoch (int): for logging purposes, allows a custom start to the
                 epoch counter when resuming training after an interruption
             log_interval (int): number of iterations between logging events
     """
     model.train()  # Turn on the train mode
+
+    # henceforth we refer to sequences as windows, as the
+    # overlap feature makes it practical to think about
+    # the sequences like a sliding window over the text.
     window_size = model.phrase_len
     total_loss = 0.
     start_time = time.time()
+
+    # We will be going over the text with a sliding window, which
+    # means the sequences will overlap. The idea is that the first
+    # and last x% of the predicted labels for a given sequence likely
+    # don't have enough bidirectional context to give an accurate
+    # prediction. This `increment` is the number of tokens that we skip
+    # to get the next window.
+    increment = round((1 - (overlap / 2)) * window_size)
+    # example: overlap = 0.2, window_size = 10
+    # 1 - (overlap / 2) = 0.9
+    # increment = 0.9 * window_size = 9
+    # In this case, there is one token of overlap at the beginning of
+    # the window and one at the end.
+    # The use of `round` is a response to floating point errors.
+    # Without it, `increment` might be something like 8.9999 in some cases.
+
     for doc_idx, document in enumerate(corpus.documents()):
+        # Padding the text, this allows the text to be cut up
+        # neatly into appropriately sized chunks to fill up a
+        # round number of batches
         padded_text = pad(document.text, window_size, overlap,
                           batch_size=batch_size,
                           pad_token=model.tokenizer.pad_token)
+
         # here huggingface may tell us that the sequence is too long
         # to pass on to BERT as is. We do not care about this as
         # we cut the text into batches afterwards.
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
             text = model.tokenizer.encode(padded_text)
+
+        # initializing targets and model inputs
         targets = torch.zeros(batch_size,
                               window_size,
                               dtype=torch.long).to(device)
@@ -61,26 +87,27 @@ def train(model, corpus, target_finder, target_indexing, optimizer,
                            window_size,
                            dtype=torch.long).to(device)
 
-        # Here we're going over the text with a sliding window with overlap.
-        # The idea is that the first x% and last x% of the predicted
-        # labels likely don't have enough bidirectional context to give an
-        # accurate prediction.
-        increment = round((1 - (overlap / 2)) * window_size)
-        # example: overlap = 0.2, window_size = 10
-        # 1 - (overlap / 2) = 0.9
-        # increment = 9
-        # the use of `round` is a response to floating point errors.
-        # Without it, `increment` might be something like 8.9999 in some cases.
-        for i in range(0, len(text)):
-            start_index = max(min(i * increment, len(text) - window_size), 0)
-            end_index = min(len(text), window_size + i * increment)
-            # TODO: prevent processing the same text segment multiple times
-            # when getting to the end of the text.
-            # UPDATE: what am I talking about? Why would the text segment be
-            # processed multiple times??
-            tw = None
+        i = 0
+        while True:
+            # For clarification: this is not an infinite loop, it's the
+            # official python syntax for a do/while. Complain to Guido.
+
+            start_index = i * increment
+            end_index = i * increment + window_size
+            if end_index > len(text):
+                break
+            # You may be wondering: "What if start_index ends up less than
+            # `increment` tokens away from the end of the window? Couldn't
+            # we miss some text if it doesn't neatly fit into a number of
+            # batches?"
+            # That can't happen, thanks to padding.
 
             def debug():
+                """
+                    This is a debugging function that is theoretically no
+                    longer useful but this specific block of code has given
+                    me a lot of trouble so I'm leaving it here just in case
+                """
                 print("data shape: {}".format(data.shape))
                 print("window number {}, in position {} of batch with "
                       "size {}".format(i, i % batch_size, batch_size))
@@ -88,6 +115,10 @@ def train(model, corpus, target_finder, target_indexing, optimizer,
                       " end index: {}, got window_size: {}".format(
                           window_size, start_index, end_index,
                           end_index - start_index))
+
+            # Every iteration of this loop just fills up the
+            # `data` matrix and its corresponding targets
+            # progressively until we fill up a batch
             try:
                 tw = get_text_window(text, window_size, start_index, end_index,
                                      pad_token=model.tokenizer.pad_token_id)
@@ -106,48 +137,38 @@ def train(model, corpus, target_finder, target_indexing, optimizer,
 
             target = target_finder(document, start_index,
                                    end_index, target_indexing)
-            # print("start_index: {} ; end_index: {}".format(
-            #     start_index, end_index))
-            # print("target: ", target)
-            target_tensor = torch.Tensor(target).to(device)
-            # print("target as Tensor: ", target_tensor)
-            # print("target size: {} ; Tensor size: {}".format(
-            #     len(target), target_tensor.size()))
-            # print("i: {} ; batch_size: {}, i % batch_size: {}".format(
-            #     i, batch_size, i % batch_size))
-            targets[i % batch_size][:target_tensor.size()[0]] = target_tensor
+            tensor_target = torch.Tensor(target).to(device)
+            targets[i % batch_size][:tensor_target.size()[0]] = tensor_target
 
-            # the fact that we do this processing only every batch_size
-            # steps means that we don't do it if the remainder of the
-            # text does not constitute a full batch. This is intentional,
-            # and the standard way of handling this case.
-            # UPDATE: the case where the remainder of the text does not
-            # constitute a full batch no longer occurs thanks to padding.
-            # print("current index in text: {}".format(i))
-            # print("remaining text windows to complete batch: {}".format(
-            #     (i + 1) % batch_size))
+            # When the batch is full, actually process it
             if (i + 1) % batch_size == 0:
                 optimizer.zero_grad()
                 output = model(data)
+
+                # Here too, the debugging code is theoretically no
+                # longer useful but this specific block of code has
+                # given me a lot of trouble
                 try:
                     loss = criterion(output, targets)
                 except Exception as e:
-                    print("targets.dtype: ", targets.dtype)
-                    print("targets: ", targets)
-                    print("output.dtype: ", output.dtype)
-                    print("output: ", output)
+                    print("targets.dtype: {}".format(targets.dtype))
+                    print("targets: {}".format(targets))
+                    print("output.dtype: {}".format(output.dtype))
+                    print("output: {}".format(output))
                     raise e
                 loss.backward()
                 torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
                 optimizer.step()
 
                 total_loss += loss.item()
+                # Resetting targets and data
                 targets = torch.zeros(batch_size,
                                       window_size,
                                       dtype=torch.long).to(device)
                 data = torch.zeros(batch_size,
                                    window_size,
                                    dtype=torch.long).to(device)
+            i += 1
 
         if doc_idx % log_interval == 0:
             cur_loss = total_loss / log_interval
@@ -178,7 +199,7 @@ def help(args, issue_description=""):
     print(issue_description)
     arg_descriptions = [
         "preloaded pickled training corpus filename",
-        "preloaded pickled validation / dev corpus filename",
+        "preloaded pickled dev corpus filename",
         "filename in which to save the model and from which to"
         " load it if not training from scratch",
         "path for all files to be written (model, stat files)",
@@ -200,8 +221,8 @@ def load_files(args):
     try:
         with open(args['--train_fname'], 'rb') as train_file:
             train_corpus = pickle.load(train_file)
-        with open(args['--val_fname'], 'rb') as val_file:
-            dev_corpus = pickle.load(val_file)
+        with open(args['--val_fname'], 'rb') as dev_file:
+            dev_corpus = pickle.load(dev_file)
         if ((args['--target_type'] == "cuid" or
              args['--target_type'] == "bin")):
             with open(cst.umls_fname, 'rb') as umls_con_file:
@@ -223,7 +244,8 @@ def load_files(args):
 
 
 def evaluate_model_performance(model, train_corpus, target_finder,
-                               target_indexing, args, dev_corpus):
+                               target_indexing, args, dev_corpus,
+                               epoch_start_time):
     (train_loss,
      train_mention_p_r_f1,
      train_doc_p_r_f1) = evaluate(model,
@@ -234,9 +256,9 @@ def evaluate_model_performance(model, train_corpus, target_finder,
                                   compute_mntn_p_r_f1=True,
                                   compute_doc_p_r_f1=True)
 
-    (val_loss,
-     val_mention_p_r_f1,
-     val_doc_p_r_f1) = evaluate(model,
+    (dev_loss,
+     dev_mention_p_r_f1,
+     dev_doc_p_r_f1) = evaluate(model,
                                 dev_corpus,
                                 target_finder,
                                 target_indexing,
@@ -244,23 +266,23 @@ def evaluate_model_performance(model, train_corpus, target_finder,
                                 compute_mntn_p_r_f1=True,
                                 compute_doc_p_r_f1=True)
 
-    print('-' * 89)
+    # print('-' * 89)
     try:
-        valid_ppl = math.exp(val_loss)
+        dev_ppl = math.exp(dev_loss)
     except OverflowError:
-        print("validation ppl too large to compute")
-        valid_ppl = "NA"
+        # print("Dev perplexity too large to compute")
+        dev_ppl = "NA"
     current_epoch_info = [str(time.time() - epoch_start_time),
                           str(train_loss),
-                          str(val_loss),
-                          str(valid_ppl),
+                          str(dev_loss),
+                          str(dev_ppl),
                           *train_mention_p_r_f1,
                           *train_doc_p_r_f1,
-                          *val_mention_p_r_f1,
-                          *val_doc_p_r_f1]
-    print(current_epoch_info)
-    print('-' * 89)
-    return current_epoch_info, val_loss
+                          *dev_mention_p_r_f1,
+                          *dev_doc_p_r_f1]
+    # print(current_epoch_info)
+    # print('-' * 89)
+    return current_epoch_info, dev_loss
 
 
 def write_results_to_file(current_epoch_info, args):
@@ -309,7 +331,7 @@ if __name__ == '__main__':
             torch.save(model, model_file)
     best_model = None
     if '--resume' not in argv:
-        column_headers = [["time", "train loss", "validation loss",
+        column_headers = [["time", "train loss", "dev loss",
                            "perplexity", "train mention precision",
                            "train mention recall", "train mention F1",
                            "train document precision", "train document recall",
@@ -322,37 +344,33 @@ if __name__ == '__main__':
             writer = csv.writer(train_stats_file, delimiter=';')
             writer.writerows(column_headers)
 
+    start_time = time.time()
+    start_info, loss =\
+        evaluate_model_performance(model, train_corpus, target_finder,
+                                   target_indexing, args, dev_corpus,
+                                   start_time)
+    write_results_to_file(start_info, args)
+
     for epoch in range(args['--epochs']):
-        # TODO: figure out why this loop never stops
         epoch_start_time = time.time()
+
+        train(model, train_corpus, target_finder,
+              target_indexing, optimizer, scheduler,
+              batch_size=args["--batch_size"],
+              overlap=args['--overlap'], epoch=epoch)
 
         current_epoch_info, loss =\
             evaluate_model_performance(model, train_corpus,
                                        target_finder,
                                        target_indexing,
-                                       args, dev_corpus)
+                                       args, dev_corpus,
+                                       epoch_start_time)
         write_results_to_file(current_epoch_info, args)
+
         if loss < best_loss:
             best_loss = loss
             best_model = model
             write_model_to_file(best_model, args)
-        # print("starting training epoch {}".format(epoch))
-        train(model, train_corpus, target_finder,
-              target_indexing, optimizer, scheduler,
-              batch_size=args["--batch_size"],
-              overlap=args['--overlap'], epoch=epoch)
-        # print("end epoch {}".format(epoch))
 
-        print("scheduler step")
+        print("End of epoch {}, advancing scheduler".format(epoch + 1))
         scheduler.step()
-
-    final_epoch_info, loss =\
-        evaluate_model_performance(model, train_corpus,
-                                   target_finder,
-                                   target_indexing,
-                                   args, dev_corpus)
-    write_results_to_file(final_epoch_info, args)
-    if loss < best_loss:
-        best_loss = loss
-        best_model = model
-        write_model_to_file(best_model, args)
